12f064bf2edc399a4138490b77c5a8ee203d61ae
Taan <tannguyen1220@gmail.com>
2022-07-15 07:42:28 +0700

    Styling post page

--- my-app/pages/blog/[id].js
+++ my-app/pages/blog/[id].js
@@ -11,14 +11,14 @@ export default function Post({ postData }) {
         <Head>
             <title>{postData.title}</title>
         </Head>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </Layout>;
+        <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+    </Layout>
 }
 
 export async function getStaticPaths() {
